8a6ba92879e243173004c0881006ef03497bf24a
josefcaique <jcaique10@gmail.com>
2025-08-15 21:37:10 -0300

    feat: devoping sensors/data route

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class SensorData(BaseModel):
+    machine_id: str
+    temperature: float
+    vibration: float
+    energy: float
+
+@app.post("sensors/data")
+async def recive_sensor_data(data: SensorData):
+    print(data.model_dump())
+    return {"status": "recived"}
